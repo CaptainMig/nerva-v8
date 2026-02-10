6d45928287a5753e16874adabe20cc034ac8bfe8
CaptainMig <miglanthony@gmail.com>
2026-02-10 00:05:41 -0500

    Update explain.js

--- api/explain.js
+++ api/explain.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
@@ -70,4 +70,4 @@ Example output:
   } catch (e) {
     return res.status(500).json({ error: "extract_failed", details: String(e) });
   }
-}
+};
